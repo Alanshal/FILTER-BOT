4b13cc10f8ffd71984421c5fda594bd038d8cb49
Alanshal <nickelodeonsonicindiayt@gmail.com>
2025-05-07 11:37:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -125,10 +125,10 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 
 
 # Shortlink Info
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False)) # Set True Or False
-SHORTLINK_URL = environ.get('SHORTLINK_URL', '')
-SHORTLINK_API = environ.get('SHORTLINK_API', '')
-TUTORIAL = environ.get('TUTORIAL', '') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set  Or False
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'https://arlinks.in/st?api=35bf30df800f56ab40299cbd938bbad3afd70f29&url=yourdestinationlink.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', '35bf30df800f56ab40299cbd938bbad3afd70f29')
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/NarutoSeriesUpdates/12') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
 
 
 # Others
