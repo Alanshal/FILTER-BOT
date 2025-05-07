1a79b35eb3968d15101d10a7f53b15a3a917e59c
Alanshal <nickelodeonsonicindiayt@gmail.com>
2025-05-07 09:37:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -112,7 +112,7 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 
 # Token Verification Info :
-VERIFY = bool(environ.get('VERIFY', False))
+VERIFY = bool(environ.get('VERIFY', True))
 VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'https://arlinks.in/st?api=35bf30df800f56ab40299cbd938bbad3afd70f29&url=yourdestinationlink.com
 ')
 VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', '35bf30df800f56ab40299cbd938bbad3afd70f29')
